bf3c5f37e00c056254d4ba1b22e7f44c2c4cd3b7
Steve Garrity <sgarrity@gmail.com>
2025-02-03 15:01:54 -0800

    update for Ruff linter

--- api.py
+++ api.py
@@ -1,35 +1,59 @@
-import aiohttp
+"""API for Lux Thermostat."""
+
 import json
 
-class LuxAPI:
+import aiohttp
 
-    def __init__(self, tokens):
+
+class LuxAPI:
+    """API for Lux Thermostat."""
+
+    def __init__(self, tokens) -> None:
+        """Initialize the API."""
         self.access_token = tokens["access_token"]
         self.refresh_token = tokens["refresh_token"]
 
-
-    async def get_user(self):
+    async def get_user(self) -> dict:
+        """Get the user."""
         url = "https://www.myluxstat.io/api/location/user"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers={"Authorization": f"Bearer {self.access_token}"}) as response:
-                res = await response.text()
-                print(res)
-                return json.loads(res)
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(
+                url, headers={"Authorization": f"Bearer {self.access_token}"}
+            ) as response,
+        ):
+            res = await response.text()
+            return json.loads(res)
 
-
-    async def get_device_state(self, device_id):
+    async def get_device_state(self, device_id) -> dict:
+        """Get the device state."""
         url = "https://www.myluxstat.io/api/device"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers={"Authorization": f"Bearer {self.access_token}", "Deviceid": device_id}) as response:
-                res = await response.text()
-                print(res)
-                return json.loads(res)
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(
+                url,
+                headers={
+                    "Authorization": f"Bearer {self.access_token}",
+                    "Deviceid": device_id,
+                },
+            ) as response,
+        ):
+            res = await response.text()
+            return json.loads(res)
 
-
-    async def set_device_state(self, device_id, state):
+    async def set_device_state(self, device_id, state) -> dict:
+        """Set the device state."""
         url = "https://www.myluxstat.io/api/device"
-        async with aiohttp.ClientSession() as session:
-            async with session.put(url, headers={"Authorization": f"Bearer {self.access_token}", "Deviceid": device_id}, json=state) as response:
-                res = await response.text()
-                print(res)
-                return json.loads(res)
+        async with (
+            aiohttp.ClientSession() as session,
+            session.put(
+                url,
+                headers={
+                    "Authorization": f"Bearer {self.access_token}",
+                    "Deviceid": device_id,
+                },
+                json=state,
+            ) as response,
+        ):
+            res = await response.text()
+            return json.loads(res)
