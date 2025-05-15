5d2ef803acd0958bc2a386d32c9090ca75172931
Steve Garrity <sgarrity@gmail.com>
2025-05-14 23:42:56 -0700

    handle 401s automatically / retry

--- luxgeo/api.py
+++ luxgeo/api.py
@@ -1,8 +1,8 @@
 """API for Lux Thermostat."""
 
 import json
+from auth import login
 from helpers import http_request
-
 
 class LuxAPI:
     """API for Lux Thermostat."""
@@ -29,6 +29,7 @@ class LuxAPI:
             "Deviceid": device_id,
         }
         res, _, _ = await self._make_request("get", url, headers=headers)
+        print(res)
         return json.loads(res)
 
     async def set_device_state(self, device_id, state) -> dict:
@@ -41,6 +42,14 @@ class LuxAPI:
         res, _, _ = await self._make_request("put", url, headers=headers, json_data=state)
         return json.loads(res)
 
-    async def _make_request(self, method, url, headers, data, json_data):
+    async def _make_request(self, method, url, headers, data=None, json_data=None):
         res, _, _ = await http_request(method, url, headers=headers, data=data, json_data=json_data)
-        return json.loads(res)
+        j = json.loads(res)
+        if "statusCode" in j and j["statusCode"] == 401:
+            tokens = await login(self.username, self.password)
+            print("here", tokens)
+            self.access_token = tokens["access_token"]
+            self.refresh_token = tokens["refresh_token"]
+            headers["Authorization"] = f"Bearer {self.access_token}"
+            return await http_request(method, url, headers, json_data=json_data)
+        return res, None, None # return res here instead of j because we already decode in the caller
--- luxgeo/main.py
+++ luxgeo/main.py
@@ -10,14 +10,14 @@ from auth import login
 dotenv.load_dotenv()
 
 async def main():
-    tokens = await login(os.getenv("USERNAME"), os.getenv("PASSWORD"))
-    # tokens = json.loads(os.getenv("TOKENS"))
-
-    print(tokens)
+    # tokens = await login(os.getenv("USERNAME"), os.getenv("PASSWORD"))
+    tokens = json.loads(os.getenv("TOKENS"))
 
     api = LuxAPI(os.getenv("USERNAME"), os.getenv("PASSWORD"), tokens)
     u = await api.get_user()
+
     device_id = u["location"][0]["devices"][0]["id"]
+    print(device_id)
     d = await api.get_device_state(device_id)
     # d["systemmode"] = 0 # 1 for on
     # await api.set_device_state(device_id, d)
